7a61066d6bb11ee306769590168c643904f4557c
Saidolimkhon Akmalov <saidolimhona12@gmail.com>
2019-07-20 19:35:54 +0100

    Step 11 and 12

--- directory.rb
+++ directory.rb
@@ -16,6 +16,9 @@ students = [
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list of students.csv"
+  puts "4. Load the list from a file of your choice"
+  puts "5. Show students from April cohort"
   puts "9. Exit"
 end
 
@@ -32,6 +35,10 @@ def process(selection)
     input_students
     when "2"
     show_students
+    when "3"
+    save_students
+    when "4"
+    load_students
     when "9"
     exit
   else
@@ -64,6 +71,25 @@ end
 
 def print_footer(students)
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 def show_students
