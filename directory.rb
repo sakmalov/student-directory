d59ee3c4b6731a5d916447c3923069a1d1478e68
Saidolimkhon Akmalov <saidolimhona12@gmail.com>
2019-07-20 18:24:23 +0100

    Step 10

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-student_count = []
+@students = []
 
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
@@ -13,53 +13,63 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+    input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+    show_students
     when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
+    exit
+  else
+    puts "I don't know what you mean, try again"
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
+  @students
 end
 
 def print_header
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
+  puts "The students of Villians Academy"
+  puts "-------------"
 end
 
+def print_students_list(students)
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
 
 def print_footer(students)
-puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
+end
+
 interactive_menu
-print_header
-print(students)
-print_footer(students)
