a9db357e8113c0e0277e38d06a7e039fad76b40f
Saidolimkhon Akmalov <saidolimhona12@gmail.com>
2019-07-20 15:40:07 +0100

    Step 9

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-student_count = 11
+student_count = []
 
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
@@ -13,6 +13,28 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -35,7 +57,9 @@ end
 def print_footer(students)
 puts "Overall, we have #{students.count} great students"
 end
+
 students = input_students
+interactive_menu
 print_header
-puts(students)
+print(students)
 print_footer(students)
